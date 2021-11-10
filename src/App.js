6a001cea571484a6c56131bea92f6271f9f7c175
wkshelley <whiteshelley14@yahoo.com>
2021-11-09 20:43:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        {" "}
+        <header className="App-header">Search for a word</header>
+        <button className="btn btn-primary">hi</button>
+        <footer>
+          Coded by Shelley White-Lyons <br /> Open-Sourced on{" "}
+          <a href="https://github.com/wkshelley/dictionary-app">GitHub</a>{" "}
+          hosted on{" "}
+          <a href="https://modest-bardeen-12d2c7.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
